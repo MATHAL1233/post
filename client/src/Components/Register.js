7625686d976949d892266a435738490ecb1e67ee
MATHAL1233 <46S195@utas.edu.om>
2025-02-25 09:46:20 +0400

    front-end

--- client/src/Components/Register.js
+++ client/src/Components/Register.js
@@ -1,7 +1,66 @@
+import { Button, Col, Input, Row,Container,Form } from "reactstrap";
 
 const Register = () => {
   return (
-    <h1>Register</h1>
+    <div>
+   <h1>Register</h1>
+    <Container>
+
+    <Form>
+
+      <Row>
+
+        <Col md={3}>
+
+        </Col>
+
+      </Row>    
+
+<Row>
+
+        <Col md={3}>          
+        Enter your name <br></br>
+        <Input type="text"></Input>
+          
+        </Col>
+
+      </Row>
+
+      <Row>
+
+        <Col md={3}>        
+         Enter your email <br></br>
+         <Input type="text"></Input>
+         
+   </Col>
+
+      </Row>
+
+      <Row>
+
+        <Col md={3}>
+         Enter your password <br></br>
+         <Input type="number"></Input>
+        </Col>
+
+      </Row>
+       <Row>
+        <Col md={3}>
+          Confirm your password <br></br>
+          <Input type="number"></Input>
+        </Col>
+       </Row>
+       <Row>
+        <Col md={3}>
+          <br></br>
+          <Button>Register</Button>
+        </Col>
+       </Row>
+      </Form>
+
+    </Container>  
+
+  </div>
   );
 };
 
