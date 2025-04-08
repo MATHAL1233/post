47ffa1285d446328bfe9ab61fe03ee6d6e38644a
MATHAL1233 <46S195@utas.edu.om>
2025-04-08 09:48:39 +0400

    server register

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 import cors from "cors";
 import express from "express";
+import bcrypt from "bcrypt";
+import UserModel from "./Models/UserModel.js";
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -9,9 +11,25 @@ app.use(cors());
 
 const connectString ="mongodb+srv://mathal:93292957@postitcluster.zxzt6sw.mongodb.net/postITDb?retryWrites=true&w=majority&appName=PostITCluster";
 
-mongoose.connect(connectString, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+mongoose.connect(connectString,);
+//API Routes
+app.post("/registerUser", async (req,res)=>{
+try{
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
+  const hashedpassword = await bcrypt.hash(password, 10);
+
+  const user = new UserModel({
+      name: name,
+      email: email,
+      password: hashedpassword,
+    });
+    await user.save();
+    res.send({ user: user, msg: "Added." });
+}catch (error){
+  console.log(error);
+} 
 });
 
 app.listen(3001, () => {
